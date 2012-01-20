4dde9e3b2de064a3dc44242cddea9fe099f038b4
Peter Horn <hornp@provideal.net>
2012-01-20 20:30:09 +0100

    i18n for period select

--- app/inputs/time_period_input.rb
+++ app/inputs/time_period_input.rb
@@ -2,6 +2,10 @@ class TimePeriodInput < SimpleForm::Inputs::Base
   def input
     @builder.text_field("#{attribute_name}_number") + " &nbsp; ".html_safe +
     @builder.select("#{attribute_name}_unit",
-      {"Tage" => 'day', "Wochen" => 'week', "Monate" => 'month', "Jahre" => 'year'})
+      I18n.translate('time_period.days')   => 'day', 
+      I18n.translate('time_period.weeks')  => 'week', 
+      I18n.translate('time_period.months') => 'month', 
+      I18n.translate('time_period.years')  => 'year'
+    )
   end
 end
