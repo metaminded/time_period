384532657680877c2cb5a0cabc4d64d7230d4e1c
Mario Albert <Mario.Albert@gmx.de>
2012-07-26 09:36:43 +0200

    html_options for input

--- app/inputs/time_period_input.rb
+++ app/inputs/time_period_input.rb
@@ -1,11 +1,13 @@
 class TimePeriodInput < SimpleForm::Inputs::Base
   def input
-    @builder.text_field("#{attribute_name}_number") + " &nbsp; ".html_safe +
-    @builder.select("#{attribute_name}_unit",
+    # attribute_name = attribute_name + '_number'
+    # "$ #{@builder.text_field(attribute_name, input_html_options)}"+ " &nbsp; ".html_safe #+
+    @builder.text_field("#{attribute_name}_number", input_html_options) + " &nbsp; ".html_safe +
+    @builder.select("#{attribute_name}_unit", {
       I18n.translate('time_period.days')   => 'day', 
       I18n.translate('time_period.weeks')  => 'week', 
       I18n.translate('time_period.months') => 'month', 
-      I18n.translate('time_period.years')  => 'year'
+      I18n.translate('time_period.years')  => 'year'}, {}, input_html_options
     )
   end
 end
